70ab1bf35774f4fa909d1df2799207b3b8e654eb
paolomococci <37364757+paolomococci@users.noreply.github.com>
2018-10-19 07:00:12 +0200

    Create Library.kt

---
+++ src/main/kotlin/local/example/demo/model/Library.kt
@@ -0,0 +1,33 @@
+/**
+ *
+ * Copyright 2018 paolo mococci
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package local.example.demo.model
+
+import org.hibernate.annotations.NaturalId
+import org.springframework.data.rest.core.annotation.RestResource
+import javax.persistence.*
+
+@Entity
+class Library {
+    @Id @GeneratedValue val id: Long = 0
+    @NaturalId @Column(nullable = false) var name: String = ""
+    @OneToOne @JoinColumn(name = "address_id")
+    @RestResource(path = "libraryAddress", rel = "address") var address: Address? = null
+    @OneToMany(mappedBy = "library") var books: List<Book>? = null
+    constructor()
+}
